c601ec460b9644d705cb3ab1f7e13e36a7d5b478
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-05-26 01:48:11 +0200

    feat: Add imprint to homepage and start adding minimal styling

--- src/frontend/components/home.tsx
+++ src/frontend/components/home.tsx
@@ -437,32 +437,55 @@ export default function Home() {
             </Modal>
           </>
         ) : (
-          <div className="pf-x-login-page pf-u-h-100 pf-u-display-flex pf-u-align-items-center pf-u-justify-content-center pf-u-flex-direction-column pf-u-p-md">
-            <Image src={logo} alt="ChatScape logo" className="pf-x-logo" />
+          <div className="pf-x-login-page pf-u-h-100 pf-u-display-flex pf-u-align-items-center pf-u-justify-content-center pf-u-flex-direction-column pf-u-p-lg">
+            <div className="pf-u-flex-1 pf-u-display-flex pf-u-align-items-center pf-u-justify-content-center pf-u-flex-direction-column pf-u-p-md">
+              <Image src={logo} alt="ChatScape logo" className="pf-x-logo" />
 
-            <Title
-              headingLevel="h3"
-              className="pf-u-mt-sm pf-u-mb-lg pf-x-subtitle"
-            >
-              Scalable Serverless Chat
-            </Title>
+              <Title
+                headingLevel="h3"
+                className="pf-u-mt-sm pf-u-mb-lg pf-x-subtitle"
+              >
+                Scalable Serverless Chat
+              </Title>
 
-            <div className="pf-x-ctas">
-              <Button
-                variant="primary"
-                icon={<DownloadIcon />}
-                className="pf-u-mr-0 pf-u-mr-sm-on-sm pf-u-mb-sm pf-u-mb-0-on-sm"
-                onClick={() => setLoggedIn(true)}
-              >
-                Download the app
-              </Button>{" "}
-              <Button
-                variant="link"
-                className="pf-u-mb-sm pf-u-mb-0-on-sm"
-                onClick={() => setLoggedIn(true)}
-              >
-                Open in browser <GlobeIcon />
-              </Button>
+              <div className="pf-x-ctas">
+                <Button
+                  variant="primary"
+                  icon={<DownloadIcon />}
+                  className="pf-u-mr-0 pf-u-mr-sm-on-sm pf-u-mb-sm pf-u-mb-0-on-sm"
+                  onClick={() => setLoggedIn(true)}
+                >
+                  Download the app
+                </Button>{" "}
+                <Button
+                  variant="link"
+                  className="pf-u-mb-sm pf-u-mb-0-on-sm"
+                  onClick={() => setLoggedIn(true)}
+                >
+                  Open in browser <GlobeIcon />
+                </Button>
+              </div>
+            </div>
+
+            <div className="pf-u-w-100 pf-u-pt">
+              <div className="pf-l-flex pf-m-justify-content-space-between pf-x-footer">
+                <div className="pf-l-flex__item">
+                  <a
+                    href="https://github.com/pojntfx/chatscape"
+                    target="_blank"
+                  >
+                    © AGPL-3.0 2023 Felicitas Pojtinger
+                  </a>
+                </div>
+                <div className="pf-l-flex__item">
+                  <a
+                    href="https://felicitas.pojtinger.com/imprint"
+                    target="_blank"
+                  >
+                    Imprint
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         )}
