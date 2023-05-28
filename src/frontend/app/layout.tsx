160ce44c71e2f8ffa3be08aa05184a531ab18d34
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-05-28 17:39:59 +0200

    feat: Add basic PWA support

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -9,9 +9,27 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>ChatScape</title>
+        <meta name="description" content="Scalable serverless chat." />
+        <meta name="author" content="Felicitas Pojtinger" />
+        <meta
+          name="keywords"
+          content="chat, serverless, aws, lambda, react, hdm-stuttgart"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://github.com/pojntfx/chatscape"
+        />
+        <meta property="og:title" content="ChatScape" />
+        <meta property="og:description" content="Scalable serverless chat." />
+        <meta property="og:image" content="/logo-light.png" />
+
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta name="description" content="Scalable Serverless Chat" />
-        <link rel="icon" href="/icon-light.png" />
+        <meta name="theme-color" content="#212427" />
+        <link rel="icon" href="/icon-light-small.png" />
+        <link rel="apple-touch-icon" href="/icon-light-small.png" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
 
       <body>{children}</body>
