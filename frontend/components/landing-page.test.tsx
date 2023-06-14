7bf08847ab39cf028a4c4cf4a7fc6832f42a5088
Felicitas Pojtinger <felicitas@pojtinger.com>
2023-06-14 23:08:40 +0200

    feat: Add support for settings endpoint to configure OIDC and API URL

--- frontend/components/landing-page.test.tsx
+++ frontend/components/landing-page.test.tsx
@@ -2,15 +2,21 @@ import { render } from "@testing-library/react";
 import { LandingPage } from "./landing-page";
 
 describe("LandingPage", () => {
-  it("renders with installPWA set", () => {
+  it("renders with installPWA set and with login set", () => {
     expect(
       render(<LandingPage installPWA={jest.fn()} login={jest.fn()} />)
     ).toMatchSnapshot();
   });
 
-  it("renders without installPWA set", () => {
-    expect(
-      render(<LandingPage installPWA={jest.fn()} login={jest.fn()} />)
-    ).toMatchSnapshot();
+  it("renders without installPWA and with login set", () => {
+    expect(render(<LandingPage login={jest.fn()} />)).toMatchSnapshot();
+  });
+
+  it("renders with installPWA set and without login set", () => {
+    expect(render(<LandingPage installPWA={jest.fn()} />)).toMatchSnapshot();
+  });
+
+  it("renders without installPWA and without login set", () => {
+    expect(render(<LandingPage />)).toMatchSnapshot();
   });
 });
