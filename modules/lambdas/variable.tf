8b1c858db773e58f97827c79514e7c834ac64459
Jakob-Waibel <jakob.waibel@ibm.com>
2023-08-11 16:51:37 +0200

    feat: Move to for-each instead of module

---
+++ modules/lambdas/variable.tf
@@ -0,0 +1,15 @@
+variable "name" {}
+
+variable "iam_role_arn" {}
+
+variable "dynamodb_table_name" {}
+
+variable "rest_api_id" {}
+
+variable "rest_api_root_resource_id" {}
+
+variable "spa_url" {}
+
+variable "authorizer_id" {}
+
+variable "rest_api_execution_arn" {}
