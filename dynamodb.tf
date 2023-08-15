74f63b628b7e1cd990eec7af57facda20aa935c0
Daniel Hiller <hillerdaniel11@gmail.com>
2023-08-15 17:30:54 +0200

    feat: Add tmp encryption for namespaces an get-contacts lambda

--- dynamodb.tf
+++ dynamodb.tf
@@ -7,6 +7,19 @@ resource "aws_dynamodb_table" "contacts" {
     name = "id"
     type = "S"
   }
+
+  attribute {
+    name = "namespace"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "NamespaceIndex"
+    hash_key        = "namespace"
+    projection_type = "ALL"
+    write_capacity  = 5
+    read_capacity   = 5
+  }
 }
 
 resource "aws_dynamodb_table" "messages" {
