5f3abbc310a07e1159be4605297d0467e5e5f46e
Jakob-Waibel <jakob.waibel@ibm.com>
2023-08-21 23:25:21 +0200

    fix: Tests for get-contacts

--- api/get-contacts/main.test.js
+++ api/get-contacts/main.test.js
@@ -19,22 +19,17 @@ const { handler } = require("./main");
 
 describe("Lambda Function Tests", () => {
   const mockEvent = {
-    httpMethod: "POST",
     body: JSON.stringify({ namespace: "your-namespace" }),
+    requestContext: {
+      authorizer: {
+        claims: {
+          "cognito:username": "test",
+        },
+      },
+    }
   };
 
   const mockContext = {};
-
-  it("should return 405 if http method is not POST", async () => {
-    const mockEvent = {
-      httpMethod: "GET",
-    };
-
-    const result = await handler(mockEvent, mockContext);
-
-    expect(result.statusCode).toBe(405);
-    expect(result.body).toBe(JSON.stringify("method not allowed"));
-  });
 
   it("should return 500 if DynamoDB query fails", async () => {
     const mockDynamoDbError = new Error("DynamoDB Error");
