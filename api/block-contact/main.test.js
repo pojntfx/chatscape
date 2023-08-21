2b5caa24e1f038f93950470ffd53e48deef10c0c
Jakob-Waibel <jakob.waibel@ibm.com>
2023-08-21 21:47:25 +0200

    test: Add block-contact tests

--- api/block-contact/main.test.js
+++ api/block-contact/main.test.js
@@ -51,4 +51,56 @@ describe("Block contact lambda", () => {
     expect(rv.statusCode).toBe(200);
     expect(rv).toMatchSnapshot();
   });
+
+  it("should handle contact not found", async () => {
+    mockQuery.mockReturnValueOnce({
+      promise: jest.fn().mockResolvedValueOnce({
+        Items: [
+          { email: "test@example.com", id: "1234" },
+          { email: "another@example.com", id: "5678" },
+        ],
+      }),
+    });
+
+    mockUpdate.mockReturnValueOnce({
+      promise: jest.fn().mockResolvedValueOnce(),
+    });
+
+    const rv = await handler({
+      httpMethod: "POST",
+      body: JSON.stringify({
+        namespace: "test",
+        email: "jakob@example.com",
+      }),
+    });
+
+    expect(rv.statusCode).toBe(404);
+    expect(rv.body).toContain("contact not found");
+    expect(rv).toMatchSnapshot();
+  });
+
+  it("should handle invalid request body", async () => {
+    const rv = await handler({
+      httpMethod: "POST",
+      body: "invalid JSON",
+    });
+
+    expect(rv.statusCode).toBe(400);
+    expect(rv.body).toContain("invalid request body");
+    expect(rv).toMatchSnapshot();
+  });
+
+  it("should handle unsupported HTTP method", async () => {
+    const rv = await handler({
+      httpMethod: "GET",
+      body: JSON.stringify({
+        namespace: "test",
+        email: "test@example.com",
+      }),
+    });
+
+    expect(rv.statusCode).toBe(405);
+    expect(rv.body).toContain("method not allowed");
+    expect(rv).toMatchSnapshot();
+  });
 });
